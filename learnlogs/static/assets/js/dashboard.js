819890e8d2183bacd5b55818b969b9a4a7296e27
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-30 18:01:15 +0300

    profile1

--- learnlogs/static/assets/js/dashboard.js
+++ learnlogs/static/assets/js/dashboard.js
@@ -5,7 +5,7 @@ function activeLink() {
   list.forEach((item) => {
     item.classList.remove("hovered");
   });
-  this.classList.add("hovered");
+  // this.classList.add("hovered");
 }
 
 list.forEach((item) => item.addEventListener("mouseover", activeLink));
