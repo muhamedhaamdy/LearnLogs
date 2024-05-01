5a08ccd4d04a4b32246632d00da049c49f89af6d
youssef2272002salah <youssef22772002salah@gmail.com>
2024-05-01 17:14:10 +0300

    editing login form

--- learnlogs/routes.py
+++ learnlogs/routes.py
@@ -51,7 +51,7 @@ def login():
             return redirect(url_for('profile1', id=student.id))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('index.html', title='Login', form=form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
